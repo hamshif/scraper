34c9c80e8a2f0ef9d426a0ba117b2d5cd1301299
Gideon Bar <gbar@marketo.com>
2018-06-18 18:18:53 +0300

    domain url

--- app/test.py
+++ app/test.py
@@ -2,7 +2,8 @@ import json
 import requests
 
 # url = 'http://localhost:5000/stories'
-url = 'http://54.236.22.180/stories'
+url = "http://gideonbar.hiring.keywee.io/stories"
+# url = 'http://54.236.22.180/stories'
 
 data = {"yo": 6}
 
